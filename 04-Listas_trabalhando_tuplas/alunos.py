ca3f62411f0334419e6af2b134c76e4ed4c27d6e
root <romeubertho@gmail.com>
2017-07-18 19:55:42 +0000

    Ren e add mais pastas

---
+++ 04-Listas_trabalhando_tuplas/alunos.py
@@ -0,0 +1,12 @@
+alunos=[]
+alunos.append("Maria")
+alunos.append("Joao")
+alunos.append("Lucas")
+alunos.append("Marcos")
+alunos.append("Edson")
+alunos.append("Carlos")
+alunos.append("Thomas")
+print(alunos)
+#for
+for aluno in alunos:
+    print(aluno)
