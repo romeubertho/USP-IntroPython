3b440eb737f9050c2a9ca33c5595866a77293e2c
root <romeubertho@gmail.com>
2017-07-20 18:20:27 +0000

    add comentarios

--- django/learning_log/learning_log/learning_logs/urls.py
+++ django/learning_log/learning_log/learning_logs/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 from . import views
 
+#Expressao regular:
+    #r -> raw text
+    #^ -> inicio do texto
+    #$ -> final do texto
+    #r'^$' -> expressao regular para texto vazio
+    
+
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+    # Testeeeee
     url(r'^teste$', views.teste, name='teste'),
 ]
