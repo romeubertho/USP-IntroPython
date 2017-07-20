11c7f24e5a860a48e820dd31748435e03e120350
root <romeubertho@gmail.com>
2017-07-20 18:07:50 +0000

    view funcionando! ( / )

---
+++ django/learning_log/learning_log/learning_logs/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # Home page
+    url(r'^$', views.index, name='index'),
+]
--- django/learning_log/learning_log/learning_logs/views.py
+++ django/learning_log/learning_log/learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """Home page"""
+    return render(request,'index.html')
