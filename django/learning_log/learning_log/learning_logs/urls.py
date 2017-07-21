7c31f716900e404023b3138cece88a502430281d
root <romeubertho@gmail.com>
2017-07-21 02:42:20 +0000

    finalizado o metodo post, falta contas de usuario

--- django/learning_log/learning_log/learning_logs/urls.py
+++ django/learning_log/learning_log/learning_logs/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from . import views
 
-#Expressao regular:
-    #r -> raw text
-    #^ -> inicio do texto
-    #$ -> final do texto
-    #r'^$' -> expressao regular para texto vazio
-    
+# Expressao regular:
+# r -> raw text
+#^ -> inicio do texto
+#$ -> final do texto
+# r'^$' -> expressao regular para texto vazio
+
 
 urlpatterns = [
     # Home page
@@ -14,5 +14,15 @@ urlpatterns = [
     # Testeeeee
     url(r'^teste/$', views.teste, name='teste'),
     # Exibir todos os topicos
-    url(r'^topics/$',views.topics,name='topics'),
+    url(r'^topics/$', views.topics, name='topics'),
+    # Pagina para um topico unico
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Get topic
+    url(r'^topics/get/(?P<topic_id>\d+)/$', views.getTopic, name='getTopic'),
+    # Pagina para adicionar um novo topico
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # Página para adicionar nova entrada
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # Página para editar entrada
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- django/learning_log/learning_log/learning_logs/views.py
+++ django/learning_log/learning_log/learning_logs/views.py
@@ -1,17 +1,108 @@
 from django.shortcuts import render
-from .models import Topic
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+# tentando enviar em Json!
+from django.http import JsonResponse
 
 # Create your views here.
+
+
 def index(request):
     """Home page"""
-    return render(request,'index.html')
+    return render(request, 'index.html')
 # testeeeeee
+
+
 def teste(request):
     """Testeeeeeee"""
-    return render(request,'teste.html')
+    return render(request, 'teste.html')
 # topics
+
+
 def topics(request):
     """Topics"""
     topics = Topic.objects.order_by('date_added')
-    context = {'topics':topics}
-    return render(request,'topics.html',context)
+    context = {'topics': topics}
+    return render(request, 'topics.html', context)
+
+
+# topic
+def topic(request, topic_id):
+    """Topic"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {
+        'topic': topic,
+        'entries': entries
+    }
+    return render(request, 'topic.html', context)
+
+
+# getTopic
+def getTopic(request, topic_id):
+    """getTopic"""
+    topic = Topic.objects.get(id=topic_id)
+    topic_list = list(topic)
+    # return render(request,context)
+    return JsonResponse(topic_list, safe=False)
+
+# Adicionar um novo topico
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {
+        'form': form
+    }
+    return render(request, 'new_topic.html', context)
+
+# Nova entrada
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'new_entry.html', context)
+
+# Editar entrada
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'edit_entry.html', context)
